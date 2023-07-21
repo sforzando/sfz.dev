5774dd9e441c62b687c7a95d130d9fd5aff60f15
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2023-07-22 02:33:11 +0900

    test: :100: revise top.en.spec.ts

--- tests/top.en.spec.ts
+++ tests/top.en.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test"
 
-test("top", async ({ page }) => {
+test("top-en-dark", async ({ page }) => {
   await page.goto("/")
   await expect(await page.screenshot()).toMatchSnapshot({ threshold: 0.3 })
 })
